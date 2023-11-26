db3fe1c07942a2b230be4e3aa1fe01a8479c9fab
✨ <renke0907@gmail.com>
2023-11-26 10:13:43 +0100

    Test(webapp): Add Tests for Web UI for posting new job using Playwright
    https://jira-se.ifi.uni-heidelberg.de/browse/ISE23-26
    
    Co-authored-by: Prakritijain11 <prakritipj@gmail.com>
    Co-authored-by: marianna <corona@stud.uni-heidelberg.de>

--- webapp/src/test/resources/tests/tests/WebUITests.spec.ts
+++ webapp/src/test/resources/tests/tests/WebUITests.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test('checkCredentials successful', async ({ page}) => {
     await page.goto('http://localhost:8080/login');
@@ -23,5 +23,38 @@ test('checkCredentials failed', async ({ page}) => {
 
     await page.waitForURL('http://localhost:8080/login?error');
 });
+
+test('postNewJob', async ({ page }) => {
+    await page.goto('http://localhost:8080/login');
+
+    await page.getByLabel('username').fill(process.env.UNAME);
+
+    await page.getByLabel('password').fill(process.env.PASSWORD);
+
+    await page.getByRole('button', { name: 'Sign in' }).click();
+
+    await page.waitForURL('http://localhost:8080');
+
+    await page.fill('#jobName', 'job1');
+
+    await page.fill('#ownersEmail', '123@web.de');
+
+    await page.fill('#boundingBox', '123.4,12.3,120.5,67.2');
+
+    await page.click('#createNewJob');
+
+    await page.waitForTimeout(2000);
+
+    const jobNameElement = await page.$('td:has-text("job1")');
+    const emailElement = await page.$('td:has-text("123@web.de")');
+
+    await expect(jobNameElement).toBeTruthy();
+    await expect(emailElement).toBeTruthy();
+
+});
+
+
+
+
 
 
