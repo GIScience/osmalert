48d7a010e8e38c9d7108327b2c21c17b46f7c773
Johannes Link <business@johanneslink.net>
2023-11-29 14:05:36 +0100

    test (webapp): Changed WebsiteTest to use live and local Osmalert webapp

--- webapp/src/test/java/org/heigit/osmalert/webapp/WebsiteTest.java
+++ webapp/src/test/java/org/heigit/osmalert/webapp/WebsiteTest.java
@@ -1,8 +1,14 @@
 package org.heigit.osmalert.webapp;
 
 import com.microsoft.playwright.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.web.server.*;
 
+@SpringBootTest(
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 public class WebsiteTest {
 	static Playwright playwright;
 	static Browser browser;
@@ -10,6 +16,9 @@ public class WebsiteTest {
 	// New instance for each test method.
 	private BrowserContext context;
 	private Page page;
+
+	@LocalServerPort
+	int port;
 
 	@BeforeAll
 	static void launchBrowser() {
@@ -39,7 +48,9 @@ public class WebsiteTest {
 	}
 
 	@Test
-	public void t() {
+	public void osmalertLogInPage() {
+		page.navigate("http://localhost:%d".formatted(port));
+		Assertions.assertThat(page.title()).isEqualTo("Please sign in");
 		page.close();
 	}
 }
