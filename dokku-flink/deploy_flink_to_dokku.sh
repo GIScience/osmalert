5b9e12a375c643644b98701768101cf46aa933d7
Johannes Link <business@johanneslink.net>
2023-12-07 10:55:31 +0100

    build (dokku-flink): Add deployment steps as workaround port mapping problems

--- dokku-flink/deploy_flink_to_dokku.sh
+++ dokku-flink/deploy_flink_to_dokku.sh
@@ -2,3 +2,13 @@
 s3d-client connect osmalert-flink
 
 git push dokku
+
+# Deployment hack to work around strange dokku behaviour concerning port mapping:
+# 1. port mapping setzen: 80:8081
+# 2. restart app
+# 3. Port mapping löschen -> Blauer Fehlerscreen
+# 4. restart app
+s3d-dokku ports:set osmalert-flink http:80:8081
+s3d-dokku ps:restart osmalert-flink
+s3d-dokku ports:clear osmalert-flink
+s3d-dokku ps:restart osmalert-flink
