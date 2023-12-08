23dbfde9962cc2c5a2ad097cab0e3298a71ed2c3
Johannes Link <business@johanneslink.net>
2023-12-08 11:42:20 +0100

    docs (dokku-flink): updated create and deploy documentation

--- dokku-flink/create_flink_on_dokku.sh
+++ dokku-flink/create_flink_on_dokku.sh
@@ -2,6 +2,7 @@ s3d-client create osmalert-flink
 s3d-dokku builder-dockerfile:set osmalert-flink dockerfile-path dokku-flink/Dockerfile
 s3d-dokku resource:limit osmalert-flink --memory 4gb
 s3d-dokku nginx:set osmalert-flink client-max-body-size 0
+s3d-dokku ports:set osmalert-flink http:80:8081 # Only suffices when https is disabled
 
 # Set all environment variables:
  s3d-client config:set osmalert-flink KAFKA_BROKER="pkc-75m1o.europe-west3.gcp.confluent.cloud:9092"
