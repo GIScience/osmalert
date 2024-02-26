3a93beaab719c939460b9a662d890d5e4c7c43de
Leon <leon.schaeffer@stud.uni-heidelberg.de>
2024-02-26 12:09:41 +0100

    Feature(flinkjobjar): Add json Property class
    https://jira-se.ifi.uni-heidelberg.de/browse/ISE23-52
    
    Co-authored-by: sbreitenbach <ef254@stud.uni-heidelberg.de>

---
+++ flinkjobjar/src/main/java/org/heigit/osmalert/flinkjobjar/model/Changeset.java
@@ -0,0 +1,12 @@
+package org.heigit.osmalert.flinkjobjar.model;
+
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.*;
+
+public class Changeset {
+	@JsonProperty("userId")
+	private int userId;
+
+	public int getUserId() {
+		return this.userId;
+	}
+}
